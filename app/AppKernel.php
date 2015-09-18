790a3ccb36bbf46600e0544b65944b9e6db1e387
wencel <wens.sumande@gmail.com>
2015-09-18 17:39:35 +0800

    oauth and readme.md

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,7 @@ class AppKernel extends Kernel {
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\OAuthServerBundle\FOSOAuthServerBundle()
+            new FOS\RestBundle\FOSRestBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
