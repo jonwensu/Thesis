29233cc93ca478f48b0ef8be8dcde6b918fa901e
Wencel Sumande <wens.sumande@gmail.com>
2017-05-27 18:18:40 +0800

    'token'

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -7,7 +7,7 @@
     $container->setParameter('database_name', substr($db["path"], 1));
     $container->setParameter('database_user', $db['user']);
     $container->setParameter('database_password', $db['pass']);
-    $container->setParameter('secret', getenv('SECRET'));
+    $container->setParameter('secret', "hatoken");
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', null);
     $container->setParameter('mailer_host', null);
